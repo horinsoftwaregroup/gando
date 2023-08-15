8542dac9010c0e28272de633f2bd46c45313c44a
navidsoleymani <navidsoleymani@ymail.com>
2023-08-15 08:56:01 +0330

    create ImageUploadTo package

---
+++ src/gando/utils/db/uploaders/images/__init__.py
@@ -0,0 +1,15 @@
+from uuid import uuid4
+import os
+
+from django.utils.deconstruct import deconstructible
+
+
+@deconstructible
+class ImageUploadTo(object):
+    def __init__(self, sub_path='images/'):
+        self.path = sub_path
+
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        filename = '{}/{}.{}'.format(uuid4().hex, uuid4().hex, ext)
+        return os.path.join(self.path, filename)
