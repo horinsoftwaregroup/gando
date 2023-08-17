16aaf5d5a3af3743e18b832c3f683b8bf251e046
navidsoleymani <navidsoleymani@ymail.com>
2023-08-17 07:03:11 +0330

    set conf

---
+++ src/gando/config/__init__.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel as Base
+from functools import lru_cache
+
+from django.conf import settings
+
+
+class Gando(Base):
+    MONITOR_KEYS: list = list()
+    DEBUG: bool = False
+
+
+@lru_cache()
+def __get_settings():
+    input_conf = settings.GANDO or {}
+    return Gando(**input_conf)
+
+
+SETTINGS = __get_settings()
