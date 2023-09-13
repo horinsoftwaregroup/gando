bbf3a3d10df5cf51b569af4431976affb985b311
navidsoleymani <navidsoleymani@ymail.com>
2023-09-13 17:57:57 +0330

    add cache flag controller to BaseService

--- src/gando/architectures/services/__base.py
+++ src/gando/architectures/services/__base.py
@@ -2,10 +2,11 @@ from abc import abstractmethod
 
 
 class BaseService:
+    reset_cache: bool = False
 
-    def __init__(self, cache_reset: bool = False, *args, **kwargs):
-        self.cache_reset = cache_reset
+    def __init__(self, cache: bool = True, *args, **kwargs):
+        self.cache: bool = cache
 
     @abstractmethod
-    def execute(self):
+    def execute(self, reset_cache=False, *args, **kwargs):
         pass
