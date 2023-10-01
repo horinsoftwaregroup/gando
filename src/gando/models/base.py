f479928127d7f7cbbec457d5330efe73c1bdcb32
navidsoleymani <navidsoleymani@ymail.com>
2023-10-01 14:16:40 +0330

    add django base models

---
+++ src/gando/models/base.py
@@ -0,0 +1,26 @@
+import uuid
+from django.db import models
+
+
+class AbstractBaseModel(models.Model):
+    uid = models.UUIDField(
+        unique=True,
+        default=uuid.uuid4,
+        editable=False,
+        db_index=True,
+    )
+    created_dt = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+    )
+    updated_dt = models.DateTimeField(
+        auto_now=True,
+        db_index=True,
+    )
+    is_active = models.BooleanField(
+        default=True,
+        db_index=True,
+    )
+
+    class Meta:
+        abstract = True
