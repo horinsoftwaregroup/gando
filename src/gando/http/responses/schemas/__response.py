9fdfe49032b4bc681d89c3bfef5a935d5d14a335
navidsoleymani <navidsoleymani@ymail.com>
2023-10-10 04:57:12 +0330

    add fields to schema

--- src/gando/http/responses/schemas/__response.py
+++ src/gando/http/responses/schemas/__response.py
@@ -5,6 +5,8 @@ from . import MessagesSchema
 
 class Response(BaseModel):
     success: bool = True
+    headers: dict = dict()
+    status_code: int = 200
     has_warning: bool = False
     monitor: dict = dict()
     messages: Optional[MessagesSchema] = MessagesSchema()
