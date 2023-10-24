b178439ddc0b504fc27812c8aeef5fbf38cd4672
navidsoleymani <navidsoleymani@ymail.com>
2023-10-24 16:21:48 +0330

    create widgets

---
+++ src/gando/schemas/structures/__init__.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Widget(BaseModel):
+    name: str
+    priority: int
+    attributes: dict
+
+
+class Section(BaseModel):
+    name: str
+    priority: int
+    widgets: List[Widget]
+
+
+class Structure(BaseModel):
+    name: str
+    sections: List[Section]
+    config: dict
