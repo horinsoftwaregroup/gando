16769222c1341ce74876138e60a6599b77713022
hydra <navidsoleymani@ymail.com>
2023-12-03 14:05:26 +0330

    add updater service

--- src/gando/architectures/services/__init__.py
+++ src/gando/architectures/services/__init__.py
@@ -4,4 +4,6 @@ from .__database_manager import BaseDataBaseManagerService
 from .__creator import BaseCreatorService
 from .__getter_creator import BaseGetterCreatorService
 
+from .__updater import BaseUpdaterService
+
 from .__getter import BaseGetterService
