77d9a07a4544bdb081bcf68f24c4249e12187405
hydra <navidsoleymani@ymail.com>
2024-02-05 17:45:24 +0330

    update image parser

--- src/gando/utils/parsers/images/__init__.py
+++ src/gando/utils/parsers/images/__init__.py
@@ -7,27 +7,57 @@ class Image:
     def __init__(self, src):
         self.__src = src
 
-        self.width = self.__open_image().width
-        self.height = self.__open_image().height
+        self._width = None
+        self._height = None
 
-        self.size = self.__src.size
+        self._size = None
 
-        self.proportion = self.__proportion()
+        self._proportion = None
 
-        self.file_format = self.__open_image().format.lower()
+        self._file_format = None
 
-        self.b64 = self.__b64()
+        self._b64 = None
 
-    __open_image_ = None
+        self._open_image = None
+
+    def get_width(self):
+        if not self._width:
+            self._width = self.__open_image().width
+        return self._width
+
+    def get_height(self):
+        if not self._height:
+            self._height = self.__open_image().height
+        return self._height
+
+    def get_size(self):
+        if not self._size:
+            self._size = self.__src.size
+        return self._size
+
+    def get_proportion(self):
+        if not self._proportion:
+            self._proportion = self.__proportion()
+        return self._proportion
+
+    def get_file_format(self):
+        if not self._file_format:
+            self._file_format = self.__src.name.split('.')[-1].lower()
+        return self._file_format
+
+    def get_b64(self):
+        if not self._b64:
+            self._b64 = self.__b64()
+        return self._b64
 
     def __open_image(self):
-        if not self.__open_image_:
-            self.__open_image_ = ImagePIL.open(self.__src)
-        return self.__open_image_
+        if not self._open_image:
+            self._open_image = ImagePIL.open(self.__src)
+        return self._open_image
 
     def __b64(self):
         try:
-            (b64width, b64height) = (self.width // 8, self.height // 8)
+            (b64width, b64height) = (self.get_width() // 8, self.get_height() // 8)
 
             mimetype = self.__open_image().get_format_mimetype()
             im_resized = self.__open_image().resize((b64width, b64height))
@@ -41,7 +71,7 @@ class Image:
             return ''
 
     def __proportion(self) -> int:
-        image_p = round(self.height / self.width, 15)
+        image_p = round(self.get_height() / self.get_width(), 15)
 
         b__p_9_16__p_2_3 = round(9 / 16 + abs(9 / 16 - 2 / 3) / 2, 15)
         b__p_2_3__p_3_4 = round(2 / 3 + abs(2 / 3 - 3 / 4) / 2, 15)
